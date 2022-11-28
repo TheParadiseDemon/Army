6b2ccae7f73a000249a84a9c54abd8c45a87114b
TheParadiseDemon <102254164+TheParadiseDemon@users.noreply.github.com>
2022-11-29 06:54:08 +1000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+typedef std::string ARRAYFORSUBJECTS[][1];
+typedef std::string ARRAYFORRULES[][4];
+
+enum Alert{};
+enum Rank{};
+
+class Soldier{
+
+};
+
+class Officer: public Soldier{
+
+};
+
+class Sergant: public Soldier{
+
+};
+
+class Ordinary: public Soldier{
+
+};
+
+int main() {
+
+    return 0;
+}
